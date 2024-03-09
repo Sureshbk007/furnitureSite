a4b326116ed39dd3a1bf1d0c8b7affb9756387d6
Suresh BK <sureshbk.contact@gmail.com>
2024-03-09 16:31:34 +0545

    minor improvements

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,14 +1,23 @@
 import validate from "../utils/validate.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+import { User } from "../models/user.model.js";
 
-const signup = (req, res) => {
-    const { fullname, email, contact, password } = req.body;
-    const errors = validate(fullname, email, contact, password);
-    if (errors) {
-      res.json(new ApiError(400, "Validation Error", errors))
-    }
-    res.json(new ApiResponse(200, req.body, "Sign up successfully"))
+const signup = async (req, res) => {
+try {
+      const { fullName, email, contact, password } = req.body;
+      const errors = validate(fullName, email, contact, password);
+      if (errors) {
+        throw new ApiError(400, "Validation Error", errors);
+      }
+      const user = await User.create(req.body);
+      res.json(new ApiResponse(200, user))
+
+} catch (error) {
+  // throw new ApiError(500, error.message)
+  console.log(error)
+  res.json({"hi":"hi"})
+}
   }
 
   export { signup }
